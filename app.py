2d75fe6f827b1a05b79f918cff8d31fb07c01480
Krishna Karthik Kaka <kakakrishnakarthik45@gmail.com>
2026-08-13 18:43:33 +0530

    Enable logo upload for QR code generation
    Modified QR code generation to allow logo upload.

--- app.py
+++ app.py
@@ -11,10 +11,12 @@ def index():
     qr_image = None
     if request.method == 'POST':
         data = request.form['data']
+        logo_file = request.files.get('logo') # Get uploaded logo
+        
         if data:
             qr = qrcode.QRCode(
                 version=1,
-                error_correction=qrcode.constants.ERROR_CORRECT_H, # H = High, needed for logo
+                error_correction=qrcode.constants.ERROR_CORRECT_H, # H = High for logo
                 box_size=10,
                 border=4,
             )
@@ -22,25 +24,3 @@ def index():
             qr.make(fit=True)
             img = qr.make_image(fill_color="black", back_color="white").convert('RGB')
             
-            # Add Logo
-            try:
-                logo = Image.open('logo.png')
-                # Resize logo to 20% of QR size
-                qr_width, qr_height = img.size
-                logo_size = qr_width // 5
-                logo = logo.resize((logo_size, logo_size))
-                
-                # Put logo in center
-                pos = ((qr_width - logo_size) // 2, (qr_height - logo_size) // 2)
-                img.paste(logo, pos)
-            except:
-                pass # If no logo.png found, just skip
-
-            buf = io.BytesIO()
-            img.save(buf, format='PNG')
-            qr_image = base64.b64encode(buf.getvalue()).decode('utf-8')
-
-    return render_template('index.html', qr_image=qr_image)
-
-if __name__ == '__main__':
-    app.run(debug=True)
